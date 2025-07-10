d5e761098dc8bc204a77888e8a6c9b134f22dc9f
macro10 <mchaletrotter22@gmail.com>
2025-07-09 22:34:02 -0400

    adjusted toast positioning for wide and small screens

--- frontend/src/components/ui/toast.jsx
+++ frontend/src/components/ui/toast.jsx
@@ -11,7 +11,9 @@ const ToastViewport = React.forwardRef(({ className, ...props }, ref) => (
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed z-[100] flex max-h-screen flex-col-reverse gap-2",
+      "mx-4 mb-4 w-[calc(100%-2rem)] md:mb-4 md:mr-4 md:w-full md:max-w-[380px]",
+      "bottom-0 right-0",
       className
     )}
     {...props} />
