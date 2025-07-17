3ed05e0fd05d2396f16174899a1c0426539d8635
macro10 <mchaletrotter22@gmail.com>
2025-07-17 19:57:06 -0400

    Fixed admin panel being intercepted by catchall route

--- backend/workouts/admin.py
+++ backend/workouts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Exercise, Workout, PerformedExercise
 
-# Register your models here.
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'muscle_group', 'description')
+    list_filter = ('muscle_group',)
+    search_fields = ('name', 'description')
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'notes')
+    list_filter = ('user', 'date')
+    search_fields = ('notes',)
+
+@admin.register(PerformedExercise)
+class PerformedExerciseAdmin(admin.ModelAdmin):
+    list_display = ('workout', 'exercise', 'sets')
+    list_filter = ('workout', 'exercise')
